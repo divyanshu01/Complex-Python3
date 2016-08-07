3e82ac51fe63e07a852415a93875af24d905b86c
dragon <divyanshu_j01@yahoo.in>
2016-08-07 23:12:58 +0530

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -1,5 +1,20 @@
 from Complex import Complex
 
-c = Complex(3,4)
-#c.inputComplex()
-c.printComplex()
+c1 = Complex(3,4)
+c2 = Complex(-4,5)
+
+#c3 = c1.addComplex(c2)
+#c3.printComplex()
+
+#c2.conjugateComplex()
+#c2.printComplex()
+
+#c4 = c1.divideComplex(c2)
+#c4.printComplex()
+
+#print(c2.modulusComplex())
+
+#c5 = c1.mulComplex(c2)
+#c5.printComplex()
+
+#c6 = c1.subComplex(c2)
