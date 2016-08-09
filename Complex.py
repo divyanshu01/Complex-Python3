953f28911473b58a5b061f6431a6c19089bbc8e7
dragon <divyanshu_j01@yahoo.in>
2016-08-09 19:54:08 +0530

    Update Complex.py

--- Complex.py
+++ Complex.py
@@ -1,10 +1,16 @@
 class Complex:
 	
 	def __init__(self, a = 0, b = 0):
+		
+		""" Parameterized constructor of the class Complex. """
+		
 		self.real = a
 		self.img = b
 	
 	def addComplex(self, complex):
+		
+		""" Function to add two complex numbers represented by Complex class objects. """
+		
 		add_result = Complex()
 		
 		add_result.real = self.real + complex.real
@@ -13,9 +19,16 @@ class Complex:
 		return add_result
 	
 	def conjugateComplex(self):
+		
+		""" Conjugates the complex number that is represented by the calling Complex class object. """
+		
 		self.img = -self.img
 	
 	def divideComplex(self, complex):
+		
+		""" Function to divide two complex numbers and return the rounded result represented by a new object
+		of the same class."""
+		
 		complex.conjugateComplex()
 		
 		div_result = self.mulComplex(complex)
@@ -25,14 +38,23 @@ class Complex:
 		return div_result
 	
 	def modulusComplex(self):
+		
+		""" Returns the modulus value of the complex number. """
+		
 		return round(((self.real ** 2 + self.img ** 2) ** (1/2.0)), 3)
 	
 	def inveseComplex(self):
+		
+		"""  Returns the rationalized result of the recirpocal of the given complex number. """
+		
 		self.conjugateComplex()
 		self.real /= self.modulusComplex()
 		self.img /= self.modulusComplex()
 	
 	def mulComplex(self, complex):
+		
+		''' FUnction to multiply two complex numbers. '''
+		
 		mul_result = Complex()
 		
 		mul_result.real = self.real * complex.real - self.img * complex.img
@@ -41,6 +63,9 @@ class Complex:
 		return mul_result
 		
 	def printComplex(self):
+		
+		''' Function to print the complex numbers on the screen. '''
+		
 		if self.real == 0:
 			if self.img == 0:
 				print(0)
@@ -58,6 +83,9 @@ class Complex:
 				print('i')
 	
 	def subComplex(self, complex):
+		
+		''' Function to return the result of the subtraction of two complex numbers. '''
+		
 		sub_result = Complex()
 		
 		sub_result.real = self.real - complex.real
